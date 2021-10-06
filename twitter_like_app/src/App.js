37623b70344158e76148d9b5309e3fd5aa5ade7b
RuslanFedorovIncode <ruslan.fedorov@incode-it.com>
2021-10-06 10:16:33 +0300

    put index.js file in component directory and desctruct props

--- twitter_like_app/src/App.js
+++ twitter_like_app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Home from './pages/Home/Home';
+import { Home } from './pages';
 import {BrowserRouter as Router, Route ,Switch} from 'react-router-dom';
 
 function App() {
--- twitter_like_app/src/pages/Post/Post.jsx
+++ twitter_like_app/src/pages/Post/Post.jsx
@@ -8,11 +8,14 @@ class Post extends Component{
     }
 
     render(){
+        
+        const {post: {title, likes, body}} = this.props
         return(
+            
             <div className='post' >
-                <div className="post_title">{this.props.post['title']}</div>
-                <div className='likes'>💚{this.props.post['likes']}</div>
-                <div className='post_body'>{this.props.post['body']}</div>
+                <div className="post_title">{title}</div>
+                <div className='likes'>💚{likes}</div>
+                <div className='post_body'>{body}</div>
             </div>
         )
     }
