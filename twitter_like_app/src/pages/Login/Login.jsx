7d75f742bca9d6c2621bbe495065f7d712bbd632
RuslanFedorovIncode <ruslan.fedorov@incode-it.com>
2021-10-08 10:00:41 +0300

    moved styles to another file

--- twitter_like_app/src/pages/Login/Login.jsx
+++ twitter_like_app/src/pages/Login/Login.jsx
@@ -2,24 +2,22 @@ import React,{Component} from "react";
 import {Grid,Paper,Avatar,Button} from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@mui/material/Typography';
+import './Login.css';
 
 class Login extends Component {
+  
   render() {
-
-    const paperStyle = { padding: 20, height: '30vh', width: 280, margin: "20px auto" };
-    const avatarStyle = { backgroundColor: '#cc9053', margin: 20 };
-    const btnstyle = { margin: '20px 0' }
     return (
       <Grid>
-        <Paper elevation={10} style={paperStyle}>
+        <Paper elevation={10} className='paperStyle'>
           <Grid align='center'>
-            <Avatar style={avatarStyle}> <LockOutlinedIcon /></Avatar>
+            <Avatar className='avatarStyle'> <LockOutlinedIcon /></Avatar>
             <h2>Login</h2>
           </Grid>
           <Typography align='center' variant='h5'>
             You are logged out!
           </Typography>
-          <Button type='submit' fullWidth variant="contained" style={btnstyle}>Login</Button>
+          <Button type='submit' fullWidth variant="contained">Login</Button>
         </Paper>
       </Grid>
     )
