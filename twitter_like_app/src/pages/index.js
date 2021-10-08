15e59e1ef337950a2df65e18b4ecc3e8da7e8b91
RuslanFedorovIncode <ruslan.fedorov@incode-it.com>
2021-10-08 09:51:49 +0300

    remade all index.js

--- twitter_like_app/src/pages/index.js
+++ twitter_like_app/src/pages/index.js
@@ -1,7 +1,3 @@
-import Home from "./Home/Home";
-import Post from "./Post/Post";
-import Navbar from "./Navbar/Navbar";
-import Login from "./Login/Login";
-import Profile_Page from "./Profile_Page/Profile_page";
-
-export {Home,Post,Navbar,Login,Profile_Page};
+export {default as Home} from './Home/Home';
+export {default as Login} from './Login/Login';
+export {default as Profile} from './Profile/Profile';
--- twitter_like_app/src/privateRoute/index.js
+++
@@ -1,3 +0,0 @@
-import PrivateRoute from "./privateRoute";
-
-export {PrivateRoute};
