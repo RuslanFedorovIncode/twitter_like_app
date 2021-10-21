8461061f9b23024e39f6a44edd415e0b2ddb0bb3
RuslanFedorovIncode <ruslan.fedorov@incode-it.com>
2021-10-21 16:24:02 +0300

    delete HomeContainer.jsx

--- twitter_like_app/src/pages/Home/Home.jsx
+++ twitter_like_app/src/pages/Home/Home.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 import './Home.css';
 import { Post } from "../../components";
+import { connect } from "react-redux";
+import { addAllPostsActionCreator } from "../../redux/PostsReducer";
 
 class Home extends Component {
-  
+
+  componentDidMount() {
+    fetch('http://localhost:3001/posts')
+      .then(res => res.json())
+      .then(posts => this.props.addAllPosts(posts))
+    
+  }
+
   render() {
     return (
+      
       <div className='main'>
         <h1>Twitter Like app</h1>
         {this.props.posts.map((post) => {
@@ -15,10 +25,24 @@ class Home extends Component {
         }
         )}
       </div>
+
     )
   }
-
 }
 
-export default Home;
+const mapStateToProps = (state) =>{
+  return{
+    posts : state.PostsReducer.posts
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return{
+    addAllPosts : (posts) => {
+      dispatch(addAllPostsActionCreator(posts))
+    }
+  }
+}
+
+export default connect (mapStateToProps,mapDispatchToProps)(Home);
 
