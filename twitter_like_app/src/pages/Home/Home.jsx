5cea669a79ea9cb4eb23038014d5529e6dcc671f
RuslanFedorovIncode <ruslan.fedorov@incode-it.com>
2021-10-21 14:52:42 +0300

    clean Home component

--- twitter_like_app/src/pages/Home/Home.jsx
+++ twitter_like_app/src/pages/Home/Home.jsx
@@ -3,25 +3,12 @@ import './Home.css';
 import { Post } from "../../components";
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: []
-    }
-  }
-
-  componentDidMount() {
-    fetch('http://localhost:3000/posts')
-      .then(res => res.json())
-      .then(result => this.setState({ posts: result }))
-  }
-
+  
   render() {
-
     return (
       <div className='main'>
         <h1>Twitter Like app</h1>
-        {this.state.posts.map((post) => {
+        {this.props.posts.map((post) => {
           return (
             <Post post={post} key={post.id} />
           )
