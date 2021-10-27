e96ba72e9b3e177d6d12c482aa94e88e802726bb
RuslanFedorovIncode <ruslan.fedorov@incode-it.com>
2021-10-27 11:49:39 +0300

    feature_upgrade_ actions.js

--- twitter_like_app/src/redux/actions.js
+++ twitter_like_app/src/redux/actions.js
@@ -1,11 +1,28 @@
-export const FETCH_POSTS = 'FETCH_POSTS';
+import { api } from "../Api";
 
-export const addAllPostsActionCreator = (posts) => ({type : FETCH_POSTS, posts});
+export const FETCH_POSTS = 'FETCH_POSTS';
+export const DELETE_POST = 'DELETE_POST';
+export const CANT_FETCH = 'CANT_FETCH';
+
+export const addAllPostsActionCreator = (posts) => ({ type: FETCH_POSTS, posts });
+export const deletePostActionCreator = (id) => ({type: DELETE_POST, id});
+export const cantFetchPostActionCreator = (error) =>({type: CANT_FETCH, error})
+
+
 
 export const fetchPostsThunk = () => {
-    return (dispatch) =>{
-    fetch(process.env.REACT_APP_URL_PATH+'/posts')
-        .then(res => res.json())
+  return (dispatch) => {
+    api.get('/posts')
         .then(posts => dispatch(addAllPostsActionCreator(posts)))
-    }
+        .catch(error => dispatch(cantFetchPostActionCreator(error.message)))
+  }
 }
+
+export const deletePostThunk = (id) => {
+  return (dispatch) => {
+    api.delete('/posts', id)
+    .then(dispatch(deletePostActionCreator(id)))
+    .catch(error => dispatch(cantFetchPostActionCreator(error.message)))
+  }
+}
+
