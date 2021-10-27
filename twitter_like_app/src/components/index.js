7baec5d9026a09d4c434ebe78b71e837ffda7353
RuslanFedorovIncode <ruslan.fedorov@incode-it.com>
2021-10-27 11:29:43 +0300

    add path for PostError

--- twitter_like_app/src/components/index.js
+++ twitter_like_app/src/components/index.js
@@ -2,3 +2,4 @@ export {default as Navbar} from './Navbar/Navbar';
 export {default as Post} from './Post/Post';
 export {default as PrivateRoute} from './privateRoute/privateRoute';
 export {default as FieldSample} from './FieldSample/FieldSample';
+export {default as PostError} from './PostError/PostError';
