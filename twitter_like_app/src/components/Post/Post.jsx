6ba7bc31f7a82f5e477e5275cf3581c11646c13c
RuslanFedorovIncode <ruslan.fedorov@incode-it.com>
2021-10-27 11:28:32 +0300

    refactored styles

--- twitter_like_app/src/components/Post/Post.jsx
+++ twitter_like_app/src/components/Post/Post.jsx
@@ -1,18 +1,31 @@
 import React, { Component } from "react";
-import "./Post.css"
+import styles from'./Post.module.css';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import Button from '@mui/material/Button';
 
 class Post extends Component {
 
   render() {
-
+    
     const { post: { title, likes, body } } = this.props
     return (
 
-      <div className='post' >
-        <div className="post_title">{title}</div>
-        <div className='likes'>💚{likes}</div>
-        <div className='post_body'>{body}</div>
+      <div className={styles.post} >
+        <div className={styles.wrapper}>
+          <div className={styles.wrapper_child}><div className={styles.post_title}>{title}</div>
+            <div className={styles.likes}>💚{likes}</div>
+            <div className={styles.post_body}>{body}</div></div>
+          <div className={styles.wrapper_child}><div>
+            <Button variant="outlined" onClick={()=>{this.props.deletePostThunk(this.props.id)}} startIcon={<DeleteForeverIcon />}>
+              Delete
+            </Button>
+          </div></div>
+        </div>
+
+
+
       </div>
+
     )
   }
 }
