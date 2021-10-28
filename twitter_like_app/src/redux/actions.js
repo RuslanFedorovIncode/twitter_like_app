33b833de227eb3ee253b6fe3d988f12ab5270553
RuslanFedorovIncode <ruslan.fedorov@incode-it.com>
2021-10-28 09:05:59 +0300

    fix actions

--- twitter_like_app/src/redux/actions.js
+++ twitter_like_app/src/redux/actions.js
@@ -1,28 +1,23 @@
 import { api } from "../Api";
 
-export const FETCH_POSTS = 'FETCH_POSTS';
-export const DELETE_POST = 'DELETE_POST';
-export const CANT_FETCH = 'CANT_FETCH';
+export const FETCH_POSTS_SUCCES = 'FETCH_POSTS_SUCCES';
+export const DELETE_POST_SUCCES = 'DELETE_POST_SUCCES';
+export const FETCH_POSTS_FAIL = 'FETCH_POSTS_FAIL';
 
-export const addAllPostsActionCreator = (posts) => ({ type: FETCH_POSTS, posts });
-export const deletePostActionCreator = (id) => ({type: DELETE_POST, id});
-export const cantFetchPostActionCreator = (error) =>({type: CANT_FETCH, error})
+export const addAllPostsActionCreator = (payload) => ({ type: FETCH_POSTS_SUCCES,payload });
+export const deletePostActionCreator = (payload) => ({type: DELETE_POST_SUCCES, payload});
+export const cantFetchPostActionCreator = (payload) => ({ type: FETCH_POSTS_FAIL, payload });
 
-
-
-export const fetchPostsThunk = () => {
-  return (dispatch) => {
-    api.get('/posts')
-        .then(posts => dispatch(addAllPostsActionCreator(posts)))
-        .catch(error => dispatch(cantFetchPostActionCreator(error.message)))
-  }
+export const fetchPostsThunk = () => (dispatch) => {
+  api.get('/posts')
+    .then(posts => dispatch(addAllPostsActionCreator(posts)))
+    .catch(error => dispatch(cantFetchPostActionCreator(error.message)))
 }
 
-export const deletePostThunk = (id) => {
-  return (dispatch) => {
-    api.delete('/posts', id)
+export const deletePostThunk = (id) => (dispatch) => {
+  api.delete('/posts', id)
     .then(dispatch(deletePostActionCreator(id)))
     .catch(error => dispatch(cantFetchPostActionCreator(error.message)))
-  }
 }
+
 
