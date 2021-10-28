387fbbcac23290db04bf173a10133758f75d2c48
RuslanFedorovIncode <ruslan.fedorov@incode-it.com>
2021-10-28 09:28:30 +0300

    fix actions

--- twitter_like_app/src/redux/actions.js
+++ twitter_like_app/src/redux/actions.js
@@ -1,23 +1,25 @@
 import { api } from "../Api";
 
 export const FETCH_POSTS_SUCCES = 'FETCH_POSTS_SUCCES';
-export const DELETE_POST_SUCCES = 'DELETE_POST_SUCCES';
 export const FETCH_POSTS_FAIL = 'FETCH_POSTS_FAIL';
+export const DELETE_POST_SUCCES = 'DELETE_POST_SUCCES';
 
-export const addAllPostsActionCreator = (payload) => ({ type: FETCH_POSTS_SUCCES,payload });
-export const deletePostActionCreator = (payload) => ({type: DELETE_POST_SUCCES, payload});
-export const cantFetchPostActionCreator = (payload) => ({ type: FETCH_POSTS_FAIL, payload });
+
+export const fetchPostsSuccesAC = (payload) => ({ type: FETCH_POSTS_SUCCES,payload });
+export const fetchPostsFailAC = (payload) => ({ type: FETCH_POSTS_FAIL, payload });
+export const deletePostSuccesAC = (payload) => ({type: DELETE_POST_SUCCES, payload});
+
 
 export const fetchPostsThunk = () => (dispatch) => {
   api.get('/posts')
-    .then(posts => dispatch(addAllPostsActionCreator(posts)))
-    .catch(error => dispatch(cantFetchPostActionCreator(error.message)))
+    .then(posts => dispatch(fetchPostsSuccesAC(posts)))
+    .catch(error => dispatch(fetchPostsFailAC(error.message)))
 }
 
 export const deletePostThunk = (id) => (dispatch) => {
   api.delete('/posts', id)
-    .then(dispatch(deletePostActionCreator(id)))
-    .catch(error => dispatch(cantFetchPostActionCreator(error.message)))
+    .then(dispatch(deletePostSuccesAC(id)))
+    .catch(error => dispatch(fetchPostsFailAC(error.message)))
 }
 
 
