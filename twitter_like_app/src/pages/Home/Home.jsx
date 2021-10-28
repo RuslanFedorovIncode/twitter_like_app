eb85e2daffb61505534ba696e2c5d0e39e92f381
RuslanFedorovIncode <ruslan.fedorov@incode-it.com>
2021-10-28 14:32:02 +0300

    fix Home

--- twitter_like_app/src/pages/Home/Home.jsx
+++ twitter_like_app/src/pages/Home/Home.jsx
@@ -1,35 +1,30 @@
 import React, { Component } from "react";
 import './Home.css';
-import { Post, ErrorComponent} from "../../components";
+import { Post, ErrorComponent, Preloader,ZeroPostsComponent} from "../../components";
 import { connect } from "react-redux";
 import { fetchPostsThunk, deletePostThunk } from "../../redux/actions";
 
 class Home extends Component {
-
   componentDidMount() {
     this.props.fetchPostsThunk()
   }
-
   render() {
     return (
       <div className='main'>
         <h1>Twitter Like app</h1>
-        {
-          this.props.error === ''
-            ?
-            this.props.posts.map((post) => {
-              return (
-                <Post
-                  post={post}
-                  key={post.id}
-                  id={post.id}
-                  deletePostThunk={this.props.deletePostThunk} />
-              )
-            })
-            :
-            <ErrorComponent
-              error={this.props.error} />
+        {this.props.isFetching && <Preloader text={'Loading posts ...'} /> }
+        {this.props.posts.map((post) => {
+          return (
+            <Post
+              post={post}
+              key={post.id}
+              id={post.id}
+              deletePostThunk={this.props.deletePostThunk} />
+          )
+        })
         }
+        {this.props.error !== '' && <ErrorComponent error={this.props.error} /> }
+        {(!this.props.posts.length && this.props.error === '') && <ZeroPostsComponent />}
       </div>
     )
   }
@@ -38,7 +33,8 @@ class Home extends Component {
 const mapStateToProps = (state) => {
   return {
     posts: state.PostsReducer.posts,
-    error: state.PostsReducer.error
+    error: state.PostsReducer.error,
+    isFetching : state.PostsReducer.isFetching
   }
 }
 
