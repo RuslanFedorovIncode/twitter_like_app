d58916baf050d86860ff19aec29f84e2ac0a02f6
RuslanFedorovIncode <ruslan.fedorov@incode-it.com>
2021-11-01 15:26:50 +0200

    Update actions.js

--- twitter_like_app/src/redux/actions.js
+++ twitter_like_app/src/redux/actions.js
@@ -4,13 +4,15 @@ export const FETCH_POSTS_SUCCES = 'FETCH_POSTS_SUCCES';
 export const FETCH_POSTS_FAIL = 'FETCH_POSTS_FAIL';
 export const DELETE_POST_SUCCES = 'DELETE_POST_SUCCES';
 export const SET_LOADING = 'SET_LOADING';
+export const SET_ADD_FORM = 'SET_ADD_FORM';
+export const ADD_POST_SUCCES = 'ADD_POST_SUCCES';
 
-
-export const fetchPostsSucces = (payload) => ({ type: FETCH_POSTS_SUCCES,payload });
+export const fetchPostsSucces = (payload) => ({ type: FETCH_POSTS_SUCCES, payload });
 export const fetchPostsFail = (payload) => ({ type: FETCH_POSTS_FAIL, payload });
-export const deletePostSucces = (payload) => ({type: DELETE_POST_SUCCES, payload});
-export const setLoading = (payload) => ({type: SET_LOADING, payload});
-
+export const deletePostSucces = (payload) => ({ type: DELETE_POST_SUCCES, payload });
+export const setLoading = (payload) => ({ type: SET_LOADING, payload });
+export const setAddForm = (payload) => ({ type: SET_ADD_FORM, payload });
+export const addPostSucces = (payload) => ({ type: ADD_POST_SUCCES, payload })
 
 export const fetchPostsThunk = () => (dispatch) => {
   dispatch(setLoading(true))
@@ -23,13 +25,17 @@ export const fetchPostsThunk = () => (dispatch) => {
       dispatch(fetchPostsFail(error.message));
       dispatch(setLoading(false));
     })
-    
-  
 }
 
 export const deletePostThunk = (id) => (dispatch) => {
   api.delete('/posts', id)
     .then(dispatch(deletePostSucces(id)))
+    .catch(error => dispatch(fetchPostsFail(error.message)))
+}
+
+export const addPostThunk = (data) => (dispatch) => {
+  api.add('/posts', data)
+    .then(response => dispatch(addPostSucces(response)))
     .catch(error => dispatch(fetchPostsFail(error.message)))
 }
 
