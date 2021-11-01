8a0cec68daff39f1259b96f5c33ddb3e97834691
RuslanFedorovIncode <ruslan.fedorov@incode-it.com>
2021-11-01 15:29:21 +0200

    Fix Some Issues

--- twitter_like_app/src/components/index.js
+++ twitter_like_app/src/components/index.js
@@ -4,4 +4,5 @@ export {default as PrivateRoute} from './privateRoute/privateRoute';
 export {default as FieldSample} from './FieldSample/FieldSample';
 export {default as ErrorComponent} from './ErrorComponent/ErrorComponent';
 export {default as Preloader} from './preloader/Preloader';
-export {default as ZeroPostsComponent} from './ZeroPostsComponent/ZeroPostsComponent'
+export {default as ZeroPostsComponent} from './ZeroPostsComponent/ZeroPostsComponent';
+export {default as ModalWindow} from './ModalWindow/ModalWindow';
