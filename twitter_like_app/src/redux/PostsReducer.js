41bfcd1dbb41056b6e260e70c938ac0208eeeb09
RuslanFedorovIncode <ruslan.fedorov@incode-it.com>
2021-11-01 15:27:07 +0200

    Update PostsReducer.js

--- twitter_like_app/src/redux/PostsReducer.js
+++ twitter_like_app/src/redux/PostsReducer.js
@@ -1,35 +1,49 @@
-import { FETCH_POSTS_SUCCES , DELETE_POST_SUCCES , FETCH_POSTS_FAIL, SET_LOADING} from "./actions";
+import { FETCH_POSTS_SUCCES, DELETE_POST_SUCCES,FETCH_POSTS_FAIL,
+SET_LOADING, SET_ADD_FORM, ADD_POST_SUCCES } from "./actions";
 
 const initialState = {
   posts: [],
-  error : '',
-  isFetching : false
+  error: '',
+  isFetching: false,
+  setAddFormValue: false
 }
 
 const PostsReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_POSTS_SUCCES: {
-      return{
-        ...state, 
-        posts : action.payload
-      }
-    }
-    case FETCH_POSTS_FAIL:{
-      return{
+      return {
         ...state,
-        error : action.payload
+        posts: action.payload
       }
     }
-    case DELETE_POST_SUCCES:{
-      return{
+    case FETCH_POSTS_FAIL: {
+      return {
+        ...state,
+        error: action.payload
+      }
+    }
+    case DELETE_POST_SUCCES: {
+      return {
         ...state,
         posts: state.posts.filter(post => post.id !== action.payload)
       }
     }
-    case SET_LOADING:{
-      return{
+    case SET_LOADING: {
+      return {
         ...state,
-        isFetching : action.payload
+        isFetching: action.payload
+      }
+    }
+    case SET_ADD_FORM: {
+      return {
+        ...state,
+        setAddFormValue: !state.setAddFormValue
+      }
+    }
+    case ADD_POST_SUCCES: {
+      return {
+        ...state,
+        posts: [...state.posts, action.payload]
       }
     }
     default:
