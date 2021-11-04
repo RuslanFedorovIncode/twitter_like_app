a3861baecdbd05278ac832430153385bf81e33fd
RuslanFedorovIncode <ruslan.fedorov@incode-it.com>
2021-11-04 12:17:32 +0200

    Fix Modal Window

--- twitter_like_app/src/components/ModalWindow/ModalWindow.jsx
+++ twitter_like_app/src/components/ModalWindow/ModalWindow.jsx
@@ -1,61 +1,25 @@
 import React, { Component } from "react";
 import styles from './ModalWindow.module.css'
-import { Dialog, DialogTitle, DialogContent, Button, Avatar } from '@mui/material';
-import AddToPhotosIcon from '@mui/icons-material/AddToPhotos';
+import { Dialog, DialogTitle, DialogContent } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
-import validationSchema from './validationSchema'
-import { Formik, Form, ErrorMessage } from 'formik';
-import { FieldSample } from "../../components";
 
-const initialValues = {
-  title: '',
-  body: ''
-}
 
 class ModalWindow extends Component {
 
-  onSubmitHandler = (values, { resetForm }) => {
-    this.props.addPostThunk(values)
-    resetForm()
-  }
   render() {
+    const { children, onClose, title } = this.props
     return (
       <Dialog
         open={true}
-        onClose={this.props.onStateChange}>
+        onClose={onClose}>
         <CloseIcon
-          className={styles.closeIcon}
-          onClick={this.props.onStateChange} />
-        <Avatar className={styles.avatar}><AddToPhotosIcon /></Avatar>
-        <DialogTitle
-          id="form_addPost"
-          className={styles.dialogTitle}
-        >Add New Post</DialogTitle>
+            className={styles.closeIcon}
+            onClick={onClose} /> 
+        <DialogTitle>
+          {title}
+        </DialogTitle>
         <DialogContent className={styles.dialogContent}>
-          <Formik
-            initialValues={initialValues}
-            onSubmit={this.onSubmitHandler}
-            validationSchema={validationSchema}>
-            {(props) => (
-              <Form>
-                <FieldSample
-                  name='title'
-                  label='Title'
-                  placeholder='Write Title'
-                  helperText={<ErrorMessage name="title" />} />
-                <FieldSample
-                  name='body'
-                  label='Text'
-                  placeholder='Write Text'
-                  helperText={<ErrorMessage name="body" />} />
-                <Button
-                  variant='contained'
-                  type='submit'>
-                  ADD POST
-                </Button>
-              </Form>
-            )}
-          </Formik>
+          {children}
         </DialogContent>
       </Dialog>
     )
